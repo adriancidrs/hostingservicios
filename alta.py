5865ee6ef023d24ffc3ad33b00abf59b59ffe46c
Adrián Cid Ramos <adrian.cid.sev@gmail.com>
2014-02-15 19:54:35 +0100

    Update alta.py
    Añadido ftp y parte de mysql

--- alta.py
+++ alta.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import MySQLdb
@@ -13,29 +14,51 @@ cursor = bd.cursor()
 cursor.execute("select userid,domain from users where userid='%s' or domain='%s';" % (nombre,dominio))
 data = cursor.fetchone()
 if data != None:
-	print "El usuario o dominio estan utilizados"
-	quit()
+        print "El usuario o dominio estan utilizados"
+        quit()
 else:
 #Creacion de carpetas del usuario/dominio
-	os.system("mkdir -p /var/www/%s" % nombre)
-	os.system("chgrp -R ftpusers /var/www/%s" % nombre)
-	os.system("chmod -R 775 /var/www/%s" % nombre)
+        os.system("mkdir -p /var/www/%s" % nombre)
+        os.system("chgrp -R ftpusers /var/www/%s" % nombre)
+        os.system("chmod -R 775 /var/www/%s" % nombre)
 #Creacion del index.html
-	os.system("echo \<html\>\<head\>\<title\>Bienvenido\</title\>\<body\>\<center\>\<h1\>La pagina %s se encuentra en construccion\</h1\>\</center\>\</body\>\</html\> > /var/www/%s/index.html" % (dominio,nombre))
+        os.system("echo \<html\>\<head\>\<title\>Bienvenido\</title\>\<body\>\<center\>\<h1\>La pagina %s se encuentra en construccion\</h1\>\</center\>\</body\>\</html\> > /var/www/%s/index.html" % (d$
 #Creacion del virtualhost
-	host=open("ficheros/plantillavirtualhost","r")
-	modif=host.read()
-	host.close()
-	modif=modif.replace("@nom@","%s" % nombre)
-	modif=modif.replace("@domain@","%s" % dominio)
+        host=open("ficheros/plantillavirtualhost","r")
+        modif=host.read()
+        host.close()
+        modif=modif.replace("@nom@","%s" % nombre)
+        modif=modif.replace("@domain@","%s" % dominio)
         host=open("/etc/apache2/sites-available/%s" % nombre,"w")
-	host.write(modif)
-	host.close
-	os.system("a2ensite %s" % nombre)
-	os.system("service apache2 reload")
+        host.write(modif)
+        host.close
+        os.system("a2ensite %s" % nombre)
+        os.system("service apache2 reload")
 
 #FTP
-	os.system("pwgen -c |cat > contra")
-	contrasena=open("contra","r")
-	cont=contrasena.read()
-	contrasena
+        os.system("pwgen -c > contra")
+        contra=open("contra","r")
+        leccontra=contra.read()
+        contra.close()
+        print "La contrasena generada para el usuario ftp es: %s" % leccontra
+        uid=open("uid","r")
+        uid2=uid.read()
+        uid.close()
+        uid2=int(uid2)
+#variable uid2 con el uid nuevo
+        uid2=uid2+1
+        uid=open("uid","w")
+        uid2=str(uid2)
+        uid.write(uid2)
+        uid.close()
+        insert="insert into users values ('%s', PASSWORD('%s'),'%s','%s',''/var/www/%s','%s','/bin/false')" % (nombre,leccontra,uid2,500,nombre,dominio)
+
+#MySQL
+        os.system("pwgen -c > contra")
+        contra=open("contra","r")
+        leccontra=contra.read()
+        contra.close()
+        print "La contrasena generada para el usuario mysql es: %s" % leccontra
+        varbase="create database bd_%s" % nombre
+        cursor.execute(varbase)
+        varusu="grant all privileges
